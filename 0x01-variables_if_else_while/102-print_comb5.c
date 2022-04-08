ac68cd730fe7e6dd74b5a92b171927e60b32a56a
Prideland-Okoi <prideland.okoi@gmail.com>
2022-04-08 14:40:27 -0700

    final task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,25 +1,48 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
-/* betty style doc for function main goes there */
 /**
- * main - Entry point
+ * main - Prints numbers between 00 to 99.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int Num1, Num2;
-    for (Num1 = 0; Num1 < 9; Num1++)
-    {
-        for (Num2 = Num1 + 1; Num2 < 10; Num2++)
-    }
- putchar((Num1 % 10) + '0')
-putchar((Num2 % 10) + '0')
-			putchar (Num1==8 && Num2==9)
-            continue;
-            putchar(',');
-            putchar('');
+int i, e, g, h, op1, op2;
+
+i = e = g = h = 48;
+while (h < 58)
+{
+	g = 48;
+	while (g < 58)
+	{
+		e = 48;
+		while (e < 58)
+		{
+			i = 48;
+			while (i < 58)
+			{
+				op1 = (h * 10) + g;
+				op2 = (e * 10) + i;
+				if (op1 < op2)
+				{
+					putchar(h);
+					putchar(g);
+					putchar(' ');
+					putchar(e);
+					putchar(i);
+					if (h == 57 && g == 56 && e == 57 && i == 57)
+						break;
+					putchar(',');
+					putchar(' ');
+				}
+				i++;
+			}
+			e++;
+		}
+		g++;
+	}
+	h++;
 }
-	return (0);
+putchar('\n');
+return (0);
+}
