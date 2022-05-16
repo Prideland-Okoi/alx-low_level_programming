5abd45ac1e366391bc3e055e8d09c662f15121f6
Prideland-Okoi <prideland.okoi@gmail.com>
2022-05-16 10:48:28 -0700

    Write a function that returns the sum of all its parameters.

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,5 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 
 /**
  * sum_them_all - returns the sum of all its parameters.
@@ -6,11 +7,6 @@
  *
  * Return: sum of its parameters.
  */
-typedef struct printer
-{
-	char *symbol;
-	void (*print)(list arg);
-}print_list;
 
 int sum_them_all(const unsigned int n, ...)
 {
