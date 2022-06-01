7151580eed9c881b05a1d945759e35de82adeb88
Prideland-Okoi <prideland.okoi@gmail.com>
2022-06-01 13:01:16 -0700

    Write a program that displays the information contained in the ELF header at the start of an ELF file.

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -6,6 +6,16 @@
 #include <unistd.h>
 #include <elf.h>
 
+void print_addr(char *ptr);
+void print_type(char *ptr);
+void print_osabi(char *ptr);
+void print_version(char *ptr);
+void print_data(char *ptr);
+void print_magic(char *ptr);
+void check_sys(char *ptr);
+int check_elf(char *ptr);
+int main(int argc, char *argv[]);
+
 /**
  * print_addr - prints address
  * @ptr: magic.
