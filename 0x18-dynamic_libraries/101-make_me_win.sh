7038fe5c8eb267083ef260a1abed29922748b448
Prideland-Okoi <prideland.okoi@gmail.com>
2022-07-12 21:26:06 -0700

    task solved

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/prideland-okoi/alx-low_level_programming/master/0x18-dynamic_libraries
-export LD_PRELOAD=tmp/nrandom.so
+wget -P /tmp/ https://github.com/prideland-okoi/alx-low_level_programming/master/0x18-dynamic_libraries/wincall.so
+export LD_PRELOAD=tmp/wincall.so
