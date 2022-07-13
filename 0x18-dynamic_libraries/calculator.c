f62231d472950d5838382c26bfd50b51235e6a00
Prideland-Okoi <prideland.okoi@gmail.com>
2022-07-13 09:14:20 -0700

    files used to solve task

---
+++ 0x18-dynamic_libraries/calculator.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+*add - takes two numbers and returns sum as an integer.
+*
+*mul - take two integers and returns as product
+*
+*div - take two integer and return a division of the integers
+*
+*mod - takes two num and returns a modulus
+*
+*sub - takes two num and give the difference
+*@a: integer
+*@b: integer
+* Return: value
+*/
+
+int add(int a, int b)
+{
+return (a + b);
+}
+
+int mul(int a, int b)
+{
+return (a * b);
+}
+
+int div(int a, int b)
+{
+return (a / b);
+}
+
+int mod(int a, int b)
+{
+return (a % b);
+}
+
+int sub(int a, int b)
+{
+return (a - b);
+}
